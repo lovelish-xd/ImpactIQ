5d26f894c7eb3354fcb339312feab39c2f3de941
Lovelish Chauhan <lovelish0010@gmail.com>
2026-09-03 01:50:33 +0530

    Add customer email to order persistence

--- demo/OrderProcessing/ns/orderProcessing/adapters/SaveOrderDB/query.sql
+++ demo/OrderProcessing/ns/orderProcessing/adapters/SaveOrderDB/query.sql
@@ -1,8 +1,9 @@
 INSERT INTO orders (
     order_id,
     customer_id,
+    customer_email,
     final_amount,
     discount_amount,
     order_status
 )
-VALUES (?, ?, ?, ?, ?);
+VALUES (?, ?, ?, ?, ?, ?);
